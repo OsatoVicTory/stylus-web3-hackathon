0babd72f0b15b38acfa5d007945d7d0d144f57d7
Osato <osatohanmenogbeide1@gmail.com>
2024-09-11 23:04:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Contract, BrowserProvider } from "ethers";
 import { Routes, Route } from 'react-router-dom';
 import abi from './contractABI.json';
 import './App.css';
-import Activity from './activity';
+import Activity from './Activity';
 import AppHome from './home';
 import Transfer from './transfer';
 import { setMessageFn, shorten, Tasks, orgTasks } from './helpers';
